37dfa06aed7e52a17db5ef3e9b65de1c0b3bac0e
LaYt <laytya@gmail.com>
2017-02-04 02:06:14 +0700

    Update Localization.lua

--- SpamThrottle/Localization.lua
+++ SpamThrottle/Localization.lua
@@ -4,7 +4,7 @@
 -- Localization Strings
 
 SpamThrottleProp = {};
-SpamThrottleProp.Version = "Vanilla_1.13";
+SpamThrottleProp.Version = "Vanilla_1.14";
 SpamThrottleProp.Author = "Mopar";
 SpamThrottleProp.AppName = "SpamThrottle";
 SpamThrottleProp.Label = SpamThrottleProp.AppName .. " version " .. SpamThrottleProp.Version;
